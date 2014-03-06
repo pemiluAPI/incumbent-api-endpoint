90993ecd7e0605e9435013172dd90b56eb907f4e
yobi.setiawan <yobi_bina_s@yahoo.co.id>
2014-03-06 19:12:38 +0700

    add initializers file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IncumbentApiEndpoint::Application.config.secret_key_base = '89f7621f890d0934902abc464d49c3c5df387000d123846673f8dc360c42bbc45216b07b31e508f11c31d885a7c363ec51f24da0e8b55e0f90a586a2e69dc2af'
